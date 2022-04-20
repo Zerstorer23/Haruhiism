3ee4c14931957c45b0870630a517292355dae982
Zerstorer23 <bismark439@gmail.com>
2022-04-19 23:13:05 -0500

    kotlin android12

--- app/src/main/java/com/haruhi/bismark439/haruhiism/activities/navigation_ui/wallpaper_setting/WallpaperSettingFragment.kt
+++ app/src/main/java/com/haruhi/bismark439/haruhiism/activities/navigation_ui/wallpaper_setting/WallpaperSettingFragment.kt
@@ -1,15 +1,6 @@
 package com.haruhi.bismark439.haruhiism.activities.navigation_ui.wallpaper_setting
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.haruhi.bismark439.haruhiism.R
 import com.haruhi.bismark439.haruhiism.activities.interfaces.IFragmentActivity
-import com.haruhi.bismark439.haruhiism.activities.navigation_ui.home.HomeViewModel
-import com.haruhi.bismark439.haruhiism.databinding.FragmentHomeBinding
 import com.haruhi.bismark439.haruhiism.databinding.FragmentWallpaperSettingBinding
 
 class WallpaperSettingFragment :
